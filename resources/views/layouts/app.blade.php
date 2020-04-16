aff8244f94dc06ecf2b33d9046c3bd00479c24b3
Nguyen Cong Quang Minh <minhncq99@github.com>
2020-04-16 09:24:09 +0700

    migrate and seed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,23 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
+                <!--Start my code-->
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <div class="navbar-nav">
+                    <div class="dropdown">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                          Discover
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="">News</a>
+                            <a class="dropdown-item" href="">Course</a>
+                            <a class="dropdown-item" href="">Topic</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <!--End my code-->
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -72,9 +89,75 @@
             </div>
         </nav>
 
+        <!--Start my code-->
+        <div class="text-center w-100 p-3 m-0 bg-primary" id="header">
+            <h1>Learn Codeing With US</h1>
+            <div class="input-group col-4 mx-auto row">
+                <input type="text" class="form-control" placeholder="Type your course want to find!">
+                <div class="input-group-append">
+                    <button class="btn btn-success" type="submit">Go</button>
+                </div>
+            </div>
+        </div>
+        <!--End my code-->
+
         <main class="py-4">
             @yield('content')
         </main>
+
+        <!--Start my code-->
+        
+        <!-- Footer -->
+        <footer class="page-footer font-small teal pt-4">
+
+            <!-- Footer Text -->
+            <div class="container-fluid text-center text-md-left">
+
+                <!-- Grid row -->
+                <div class="row">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Thông tin thành viên</h5>
+                    <ul>
+                        <li>Nguyễn Công Quang Minh - 1751010086</li>
+                        <li>Hoàng Phạm Minh Trí - </li>
+                    </ul>
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mb-md-0 mb-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Mô tả Webstite</h5>
+                    <li>Là webstite được tạo ra bằng ngôn ngữ PHP thông qua FrameWork Laravel 5.8. 
+                        Website với nội dung các bài học giúp các bạn hệ thống lại kiến thức và học thêm các kiến 
+                        thức mới về lập trình cũng như cung cấp các bản tin mới về công nghệ lập trình.</li>
+
+                </div>
+                <!-- Grid column -->
+
+                </div>
+                <!-- Grid row -->
+
+            </div>
+            <!-- Footer Text -->
+
+            <!-- Copyright -->
+            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                <strong>Sản phẩm của nhóm 7</strong>
+            </div>
+            <!-- Copyright -->
+
+        </footer>
+        <!-- Footer -->
+
+        <!--End my code-->
     </div>
 </body>
 </html>
